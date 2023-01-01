4061ea4d339994e427710296967fdf40cf0fcc0f
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2023-01-01 18:24:48 +0100

    Add Card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,22 @@
 import React, {useState} from 'react';
 
-function Card() {
+function Card({cards, setCards}) {
+    const defaultLabel = 'DefaultLabel';
+    const defaultTitle = 'DefaultTitle';
+    const [label, setLabel] = useState(defaultLabel);
+    const [title, setTitle] = useState(defaultTitle);
+
+    const handleRemove = () => {
+        let newCards = [...cards];
+        newCards.pop();
+        setCards(newCards);
+    }
+    
+    return (<div>
+        <p>{label}</p>
+        <p>{title}</p>
+        <button onClick={handleRemove}>Remove</button>
+        </div>)
 }
 
 export default Card;
