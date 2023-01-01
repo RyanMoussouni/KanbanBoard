127748f94272d921c37bfc7122930f9c8f9a9f63
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2023-01-01 22:00:43 +0100

    Refactor Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,28 +1,33 @@
 import React, {useState} from 'react';
+import CardDetails from './CardDetails';
 
 //todo: understand how to use something else than idx
 function Card({index, cards, setCards}) {
     const defaultLabel = 'DefaultLabel';
     const defaultTitle = `Card n° ${index}`;
+    const defaultCardDetailsVisibility = false;
+
     const [label, setLabel] = useState(defaultLabel);
     const [title, setTitle] = useState(defaultTitle);
+    const [isCardDetailsVisible, setIsCardDetailsVisible] = useState(defaultCardDetailsVisibility);
 
-    const handleDisplay = () => {
-        console.log("Display!");
+    const handleCardDetailsDisplay = () => {
+        setIsCardDetailsVisible(!isCardDetailsVisible);
     };
 
-    const handleRemove = () => {
+    const handleCardRemoval = () => {
         let newCards = [...cards];
         newCards.pop(index);
         setCards(newCards);
     };
     
     return (<div>
-                <div onClick={handleDisplay}>
+                <div onClick={handleCardDetailsDisplay}>
                     <p>{label}</p>
                     <p>{title}</p>
                 </div>
-                <button onClick={handleRemove}>Remove</button>
+                <button onClick={handleCardRemoval}>Remove</button>
+                <CardDetails/>
             </div>);
 }
 
