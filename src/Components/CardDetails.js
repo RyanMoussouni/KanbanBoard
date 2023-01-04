fcaf5956559be6a4d2a930ad3e16adf58ecdc2b9
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2023-01-04 08:25:37 +0100

    Add PopUp

--- src/Components/CardDetails.js
+++ src/Components/CardDetails.js
@@ -2,8 +2,14 @@ import React, { useState } from 'react';
 
 function CardDetails (props) {
   const [isPopUpVisible, setIsPopUpVisible] = useState(false);
+  const [isDescriptionEditable, setIsDescriptionEditable] = useState(false);
+
   const togglePopUp = () => {
     setIsPopUpVisible(!isPopUpVisible);
+  };
+  //TODO: make this interact with CSS
+  const editDescription = () => { 
+    setIsDescriptionEditable(!isDescriptionEditable);
   };
 
   return (
@@ -12,8 +18,12 @@ function CardDetails (props) {
       {isPopUpVisible && (
         <div className="popup">
           <div className="popup-inner">
-            <h1>PopUp Title</h1>
-            <p>PopUp content goes here</p>
+            <h1 contentEditable>PopUp Title</h1>
+            <h2> Description </h2>
+            <button onClick={editDescription}>Edit</button> 
+                <textArea onclick={editDescription} className='popup-description' placeholder='Description Content goes here'></textArea>
+            <h2> Activity </h2>
+                <textArea className='popup-activity' placeholder='Activity Content goes here'></textArea>
             <button onClick={togglePopUp}>Close PopUp</button>
           </div>
         </div>
