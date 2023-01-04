7e8efd5057c6405dece73ee8769e0e3f6976681e
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2023-01-04 09:25:11 +0100

    Add multiple lists feature
    The code is not very clean, it should be refactored at some point

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import List from './Components/List';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [isList, setIsList] = useState(false);
+  const renderList = () => {
+    let toRender = (<button onClick={() => setIsList(true)}> Add a list </button>)
+    if (isList)
+    {
+      toRender = (<List/>);
+    }
+
+    return toRender;
+  };
+
   return (
     <div className="App">
-      <List/>
+      <div>
+        <List/>
+      </div>
+      <div>
+        {renderList()}
+      </div>
     </div>
   );
 }
