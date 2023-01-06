e7d167866626fdce70818db9eda8e89a87d077f9
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2023-01-06 07:23:05 +0100

    Add MainPanel

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import './App.css';
-import List from './Components/List';
+import MainPanel from './Components/MainPanel';
 import React, { useState } from 'react';
 
 
 function App() {
-  const [isList, setIsList] = useState(false);
-  const renderList = () => {
-    let toRender = (<button onClick={() => setIsList(true)}> Add a list </button>)
-    if (isList)
-    {
-      toRender = (<List/>);
-    }
-
-    return toRender;
-  };
-
-  return (
-    <div className="App">
-      <div>
-        <List/>
-      </div>
-      <div>
-        {renderList()}
-      </div>
-    </div>
-  );
+  return (<MainPanel/>);
 }
 
 export default App;
